3a2cfc2b73a8c0b052efd34dfb3497ef0d07c7f2
Vahid Shirbisheh <shirbisheh@gmail.com>
2021-10-07 11:54:31 +0330

    Adding data structre queue (built based on linkedlists).

---
+++ queue/queue.py
@@ -0,0 +1,15 @@
+"""
+Here we build queue class independent of linkedlist class.
+"""
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Queue:
+    def __init__(self, *args):
+        pass
+    
---
+++ queue/queuell.py
@@ -0,0 +1,52 @@
+"""
+Here we build queue class using linkedlist class. Afterwards we will build queue class independently.
+"""
+import sys
+sys.path.append('C:\\Users\\acer\\basic_algorithms_python\\linked_list')
+from linkedlist import LinkedList
+
+
+class Queuell:
+    def __init__(self, *args):
+        self.body = LinkedList(*args)
+        self.first = self.body.head
+        self.last = self.body.tail
+        self.length = self.body.length
+
+    def __eq__(self, other):
+        return self.body == other.body
+
+    def __str__(self):
+        temp_string = "(first)"
+        temp = self.first
+        while True:
+            if temp:
+                temp_string = temp_string + str(temp.value)
+            else:
+                temp_string = temp_string + str(temp)
+            if temp == self.last:
+                temp_string = temp_string + "(last)"
+                break
+            else:
+                temp_string = temp_string + " -> "
+            temp = temp.next
+        if self.length > 0:
+            temp_string = temp_string + " -> None"
+        return temp_string
+
+    def enqueue(self, value):
+        self.body.append(value)
+        self.last = self.body.tail
+        if self.length == 0:
+            self.first = self.body.head
+        self.length += 1
+        return True
+
+    def dequeue(self):
+        temp = self.body.popFirst()
+        if self.length > 0:
+            self.length -= 1
+            self.first = self.body.head
+            if self.length == 0:
+                self.last = self.body.tail  # which is equal to None
+        return temp
